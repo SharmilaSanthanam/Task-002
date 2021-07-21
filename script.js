89e37fcc5c35de986fe6cddf2d4040d5774a1f30
SharmilaSanthanam <87688938+SharmilaSanthanam@users.noreply.github.com>
2021-07-21 16:19:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+//XMLHttpRequest(XHR) -> are used to interact with servers through API
+//1.creating an XHR object
+//new keyword
+var request=new XMLHttpRequest();
+//2.opening a connection (specify the URL)
+request.open('GET','https://restcountries.eu/rest/v2/all',true);
+//3.sending a connection
+request.send();
+//4.once the server responded successfully then we have to process the data
+request.onload=function(){
+    var data=JSON.parse(this.response);
+    console.log(data);
+    for(var i=0;i<250;i++){
+        
+        console.log(data[i].name+" "+data[i].capital+" "+data[i].flag);
+        //console.log(data[i].capital);
+        }
+    
+}
+
